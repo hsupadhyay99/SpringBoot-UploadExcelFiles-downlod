d59384889d66c53a0f6289907e852b52a17ff1c4
Hari Shankar Upadhyay <hsupadhyay99@gmail.com>
2022-03-28 23:34:44 +0530

    First

---
+++ src/main/java/com/hari/spring/files/excel/repository/TutorialRepository.java
@@ -0,0 +1,8 @@
+package com.hari.spring.files.excel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hari.spring.files.excel.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+}
